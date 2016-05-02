e3a5d19c031bd2346ea192c5ee9645bbeca0ef0b
Mark Oliver <moliver777@googlemail.com>
2016-05-02 18:04:59 +0100

    box-office added to user levels array

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   BOX_OFFICE = "BOX_OFFICE"
   REGULAR = "REGULAR"
   DEVELOPER = "DEVELOPER"
-  LEVELS = [REGULAR,DEVELOPER]
+  LEVELS = [BOX_OFFICE,REGULAR,DEVELOPER]
   
   def self.encrypt password
     Encryptor::Aes256.new.digest password, 0
